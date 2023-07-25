9bf5bcda178f20d04064ba99639fcc5aa9e3b6f8
Horakhsh Sharifi <horakhshsharifi1@gmail.com>
2023-07-25 12:44:13 +0500

    ninth commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const btn = document.getElementById("btn");
+const copyBtn1 = document.getElementById("copy-btn-1");
+const copyBtn2 = document.getElementById("copy-btn-2");
 let inputfelid1 = document.getElementById("inp-1");
 let inputfelid2 = document.getElementById("inp-2");
 
@@ -27,4 +29,12 @@ btn.addEventListener("click", function () {
   }
 
   inputfelid2.value = password;
+});
+copyBtn1.addEventListener("click", function () {
+  inputfelid1.select();
+  document.execCommand("copy");
+});
+copyBtn2.addEventListener("click", function () {
+  inputfelid2.select();
+  document.execCommand("copy");
 });
